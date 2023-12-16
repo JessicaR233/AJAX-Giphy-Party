bb6a313a54783a230dc6a230dcd6a6d833e625a6
jessica <laylaruan0927@gmail.com>
2023-12-15 19:02:25 -0500

    append gifs

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 console.log("Let's get this party started!");
 
 const $searchInput = $('#searchterm');
+const $gifArea = $('.gif-area');
 //build a function to append gif to gif area
 function addGIF(res)   {
+    //find out amount of results for random pick
+    let numResults = res.data.length;
+    // console.log(numResults);
+    if(numResults)  {
+        let randomIdx = Math.floor(Math.random()*numResults);
+        // console.log(randomIdx);
+        //create new img element
+        let newGIF = document.createElement('img');
+        newGIF.src = res.data[randomIdx].images.original.url;
+        // console.log(newGIF);
+        //append img to gif area
+        $gifArea.append(newGIF);
+        // console.log($gifArea);
+    }
     
     
 }
